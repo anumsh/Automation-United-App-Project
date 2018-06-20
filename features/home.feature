69fd03cd74c0cf7b7c3916d0d55b663bb7c61560
anum <anumsharma09@gmail.com>
2018-06-19 23:33:02 -0700

    feat: Add new scanerios of United App

--- features/home.feature
+++ features/home.feature
@@ -1,7 +1,32 @@
 Feature: Home Screen feature
 
-  @home
-  Scenario: checking navigation in Horizontal Scroll View
+  @home_1
+  Scenario Outline: checking navigation in Horizontal Scroll View
     Given I land to Home Screen and verify Home tab is displayed
-    When I click on "Boarding Passes" in Horizontal Scroll View
-    Then I verify card title text is equal "No boarding passes"
+    When I click on "<tab_name>" in Horizontal Scroll View
+    Then I verify card title text is equal "<arg>"
+    Examples:
+      | tab_name        | arg                |
+      | Boarding Passes | No boarding passes |
+      | Flight Status   | No active flights  |
+
+    @signin
+    Scenario: User should not be able to sign in without valid data to login and password fields
+      Given I land on the home screen and verify Home tab is displayed
+      When I tap on Welcome on United section
+      Then I tap Sign in button
+      Then error message "Account number and password are required." should be displayed
+
+      @entertainment
+      Scenario: user should able to open entertainment screen
+        Given I land on the home screen and verify Home tab is displayed
+        When I click on Entertainment section
+        Then "Personal device entertainment" should be displayed on Entertainment page
+
+      @converter
+      Scenario: Using left to right swipe user should be able to use currency converter
+        Given I land on the home screen and verify Home tab is displayed
+        When I swipe screen to the "right" direction
+        And I look for menu item "Currency converter" , swipe to find if it necessary and then tap
+        Then I enter "10000" as amount I want to exchange
+        Then I click currency to convert from
--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -2,6 +2,9 @@ Given(/^I land to Home Screen and verify Home tab is displayed$/) do
   text("Home")
   #sleep
 end
+Given("I land on the home screen and verify Home tab is displayed") do
+  puts "landed to home screen"
+end
 
 When(/^I click on "([^"]*)" in Horizontal Scroll View$/) do |tab_name|
   text(tab_name).click
@@ -9,4 +12,48 @@ end
 
 Then(/^I verify card title text is equal "([^"]*)"$/) do |arg|
    expect(find_element(id:"main_menu_card_default_fragment_card_view_title").text).to eq(arg)
+end
+
+When(/^I tap on Welcome on United section$/) do
+  find_element(id:"main_menu_mp_title").click
+end
+
+Then(/^I tap Sign in button$/) do
+  find_element(id:"main_menu_mp_signin_button").click
+end
+
+Then(/^error message "([^"]*)" should be displayed$/) do |text|
+  expect(find_element(id:"message").text).to eq(text)
+end
+
+When(/^I click on Entertainment section$/) do
+  find_element(id:"main_menu_entertainment").click
+end
+
+Then(/^"([^"]*)" should be displayed on Entertainment page$/) do |text|
+  expect(find_element(id:"inflight_header").text).to eq(text)
+end
+
+When(/^I swipe screen to the "([^"]*)" direction$/) do |direction|
+  if direction == "right"
+    swipe(:start_x =>0.01, :start_y => 0.5, :end_x => 0.99, :end_y => 0.5 , :duration => 900).perform
+  elsif direction =="left"
+    swipe(:start_x =>0.99, :start_y => 0.5, :end_x => 0.01, :end_y => 0.5 , :duration => 900).perform
+  end
+end
+
+And(/^I look for menu item "([^"]*)" , swipe to find if it necessary and then tap$/) do |text|
+  swipe(:start_x =>0.5, :start_y => 0.99, :end_x => 0.05, :end_y => 0.2, :duration => 900)
+  menu_item = text(text)
+  menu_item.click
+end
+
+Then(/^I enter "([^"]*)" as amount I want to exchange$/) do |amount|
+  amount_field =find_element(id:"editAmount")
+  amount_field.click
+  amount_field.send_keys amount
+end
+
+Then(/^I click currency to convert from$/) do
+find_element(id:"fromCurrencyItems").click
 end
